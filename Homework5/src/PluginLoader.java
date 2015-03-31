0c52ad13d45a7837936ce81646f8b7c268c2cbc6
Gordon Hazzard <hazzargm@rose-hulman.edu>
2015-03-31 16:00:24 -0400

    new commit

--- Homework5/src/PluginLoader.java
+++ Homework5/src/PluginLoader.java
@@ -5,19 +5,25 @@ import java.util.ArrayList;
 public class PluginLoader {
 	
 	public PluginLoader() {
-		File dir = new File("PluginFolder");
-		File[] filesList = dir.listFiles();
-		String files = "";
-		for (File file : filesList) {
-			if(file.isFile()) {
-				files = files + file.getName() + "\n"; 
-			}
-		}
-		System.out.println(dir.getAbsolutePath());
+		ArrayList<String> pluginNames =fetchPlugins();
+		System.out.println(pluginNames);
 	}
 	
 	public ArrayList<Object> loadPlugins() {
 		return null;
+	}
+	
+	public ArrayList<String> fetchPlugins() {
+		ArrayList<String> pluginNames = new ArrayList<String>();
+		File dir = new File(System.getProperty("user.dir") + "/PluginFolder");
+		File[] filesList = dir.listFiles();
+		for (File file : filesList) {
+			if(file.isFile()) {
+				pluginNames.add(file.getName());
+				System.out.println(file);
+			}
+		}
+		return pluginNames;
 	}
 
 }
